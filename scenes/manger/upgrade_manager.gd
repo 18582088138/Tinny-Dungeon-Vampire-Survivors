406809ca2fd4e7f657b17344b25d1084de221703
Kunda.Xu <kunda.xu@intel.com>
2026-08-16 17:52:50 +0800

    feat: Prevent Ability from being chosen twice

--- scenes/manger/upgrade_manager.gd
+++ scenes/manger/upgrade_manager.gd
@@ -11,7 +11,6 @@ func _ready():
 	
 	
 func apply_upgrade(upgrade: AbilityUpgrade):
-
 	var has_upgrade = current_upgrades.has(upgrade.id)
 	if !has_upgrade:
 		current_upgrades[upgrade.id] = {
@@ -21,6 +20,11 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
 		
+	if upgrade.max_quantity > 0:
+		var current_quantity = current_upgrades[upgrade.id]["quantity"]
+		if current_quantity == upgrade.max_quantity:
+			upgrade_pool = upgrade_pool.filter(func (pool_upgrade): return pool_upgrade.id != upgrade.id)
+		
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 		
 		
@@ -28,7 +32,10 @@ func pick_upgrades():
 	var chosen_upgrades: Array[AbilityUpgrade] = []
 	var filtered_upgrades = upgrade_pool.duplicate()
 	for i in 2:
+		if filtered_upgrades.size() == 0:
+			break
 		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+
 		chosen_upgrades.append(chosen_upgrade)
 		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
 	return chosen_upgrades
