e885ec2f3a10e786a8c0946ba0f1ed443f367f78
Kunda.Xu <kunda.xu@intel.com>
2026-08-29 09:01:36 +0800

    feat: Add theme & text font (Rockboxcond12.ttf)

--- scenes/game_object/experience_vial/experience_vial.gd
+++ scenes/game_object/experience_vial/experience_vial.gd
@@ -20,7 +20,7 @@ func  tween_collect(percent: float, start_postion: Vector2):
 	rotation = lerp_angle(rotation, target_rotation, 1 - exp(-2 * get_process_delta_time()))
 
 func collect():
-	#GameEvents.emit_experience_vial_collected(1)
+	GameEvents.emit_experience_vial_collected(1)
 	queue_free()
 
 
