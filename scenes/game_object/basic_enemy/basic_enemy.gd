6efef8684f5cb41663087639a24761206457187f
Kunda.Xu <kunda.xu@intel.com>
2026-08-31 10:48:36 +0800

    feat: Add sfx aduio for enemy

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 @onready var visuals = $Visuals
 @onready var velocity_componet = $VelocityComponet
 
+
+func _ready():
+	$HurtboxComponent.hit.connect(on_hit)
+
+
 func _process(delta):
 	velocity_componet.accelerate_to_player()
 	velocity_componet.move(self)
@@ -10,3 +15,7 @@ func _process(delta):
 	var move_sign = sign(velocity.x)
 	if move_sign != 0 :
 		visuals.scale = Vector2(move_sign, 1)
+		
+		
+func on_hit():
+	$HitRandomAudioPlayerComponent.play_random()
